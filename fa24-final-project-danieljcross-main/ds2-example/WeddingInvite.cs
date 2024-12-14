d6f4ec57b8e9310baf72cdc661be2d7e29baf8ee
danieljcross <danielcross2872@gmail.com>
2024-12-13 20:14:00 -0700

    Finish Example 2

--- fa24-final-project-danieljcross-main/ds2-example/WeddingInvite.cs
+++ fa24-final-project-danieljcross-main/ds2-example/WeddingInvite.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 class WeddingInvite{
     public static HashSet<string> CreateUniqueInviteList(List<string> list1, List<string> list2){
 
+        // Creates a set union between the two lists
         HashSet<string> uniqueInviteSet = new HashSet<string>(list1);
         uniqueInviteSet.UnionWith(list2);
         return uniqueInviteSet;
--- fa24-final-project-danieljcross-main/ds2-example/WeddingInviteTests.cs
+++ fa24-final-project-danieljcross-main/ds2-example/WeddingInviteTests.cs
@@ -7,15 +7,20 @@ class WeddingInviteTests{
     }
 
     private static void TestUniqueInviteList(){
-        // Test case
+
+        // Test case 1
         // Scenario: Two lists each have a name in common. We want a list of all unique names.
         // Expected output: Jessica, Dave, Vincent, Drew, Hannah, Emily, Ezra (order may vary)
         Console.WriteLine("Test 1");
+        
+        // Create the two lists
         List<string> danielsList = new List<string> { "Jessica", "Dave", "Vincent", "Drew" };
         List<string> emmasList = new List<string> { "Hannah", "Vincent", "Emily", "Ezra" };
 
+        // Get a list of just the unique names
         HashSet<string> uniqueInviteList = WeddingInvite.CreateUniqueInviteList(danielsList, emmasList);
 
+        // Print all of the unique names
         int listCount = uniqueInviteList.Count;
         foreach (string name in uniqueInviteList){
             Console.Write(name);
