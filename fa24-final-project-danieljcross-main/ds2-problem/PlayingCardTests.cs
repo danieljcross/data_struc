2edcf4d83c1ae1a5466d52c73094ab4bdbf306b7
danieljcross <danielcross2872@gmail.com>
2024-12-13 20:30:04 -0700

    Small fix

--- fa24-final-project-danieljcross-main/ds2-problem/PlayingCardTests.cs
+++ fa24-final-project-danieljcross-main/ds2-problem/PlayingCardTests.cs
@@ -10,5 +10,10 @@ class PlayingCardTests
         List<string> cardsInHand = new List<string>{"A", "2", "3", "7", "8", "9", "10", "J", "Q", "K", "K"};
         HashSet<string> cardsSet = PlayingCardChecker.CreateCardSet(cardsInHand);
         List<string> missingCards = PlayingCardChecker.GetMissingCards(cardsSet);
+
+        // Write some test cases here
+
+
+        
     }
 }
