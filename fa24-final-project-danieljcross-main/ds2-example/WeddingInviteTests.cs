396afa79bf7a8abd244839ebf3a111f4368c0ed3
danieljcross <danielcross2872@gmail.com>
2024-12-13 20:04:04 -0700

    Small Fix

--- fa24-final-project-danieljcross-main/ds2-example/WeddingInviteTests.cs
+++ fa24-final-project-danieljcross-main/ds2-example/WeddingInviteTests.cs
@@ -10,15 +10,22 @@ class WeddingInviteTests{
         // Test case
         // Scenario: Two lists each have a name in common. We want a list of all unique names.
         // Expected output: Jessica, Dave, Vincent, Drew, Hannah, Emily, Ezra (order may vary)
-
+        Console.WriteLine("Test 1");
         List<string> danielsList = new List<string> { "Jessica", "Dave", "Vincent", "Drew" };
         List<string> emmasList = new List<string> { "Hannah", "Vincent", "Emily", "Ezra" };
 
         HashSet<string> uniqueInviteList = WeddingInvite.CreateUniqueInviteList(danielsList, emmasList);
 
-        Console.WriteLine("Unique Invite List:");
+        int listCount = uniqueInviteList.Count;
         foreach (string name in uniqueInviteList){
-            Console.WriteLine(name);
+            Console.Write(name);
+            listCount -= 1;
+            if (listCount > 1){
+                Console.Write(", ");
+            }
+            else if (listCount == 1){
+                Console.Write(", and ");
+            }
         }
     }
 }
